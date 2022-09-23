5f3e983f1272107412c797dbc93abbd3ee89a264
doveh3617 <113809302+doveh3617@users.noreply.github.com>
2022-09-22 22:47:01 -0400

    Add files via upload
    Sept 22- Travel Expense

---
+++ P1HW2_Travel_DoveHayley.py
@@ -0,0 +1,34 @@
+#This program will calculate and display travel expenses
+#September 22, 2022
+#CTI-110 P1HW2 - Travel Expense
+#Hayley Dove
+#
+budget = int('2000')
+location = 'Atlanta'
+gas = int('300')
+hotel = int('400')
+food = int('250')
+total = int(gas + hotel + food)
+balance = int(budget - total)
+print ('This program calculates and displays travel expenses\n')
+print ('Enter Budget:', budget)
+print ()
+print ('Enter your travel destination:', location)
+print ()
+print ('How much do you think you will spend on gas?', gas)
+print ()
+print ('Approximately, how much will you need for acoomodation/hotel?', hotel)
+print ()
+print ('Last, how much do you need for food?', food)
+print ()
+print ('------------Travel Expenses------------')
+print ('Location:', location)
+print ('Initial Budget:', budget)
+print ()
+print ('Fuel:', gas)
+print ('Accomodation:', hotel)
+print ('Food:', food)
+print ()
+print ('Remaining Balance:', balance)
+
+
